a1338638eec4f18cb68daf7815da63cbd95d3ed0
warrior_spb <ivanovandrew2004@gmail.com>
2025-12-18 01:47:47 +0400

    Load questions based on synonyms too #1

--- Bingo.Application/GetBingoQuestion/GetBingoQuestionCommand.cs
+++ Bingo.Application/GetBingoQuestion/GetBingoQuestionCommand.cs
@@ -2,8 +2,9 @@
 
 namespace Bingo.Application.GetBingoQuestion;
 
-public class GetBingoQuestionCommand : IRequest
+public record GetBingoQuestionCommand : IRequest
 {
     public int BingoId { get; init; }
     public long ChatId { get; init; }
+    public bool RetryIfNoQuestions { get; init; } = true;
 }
